da9ab7193b40fbf9bc3b3aa44b1f926d79a6ce90
Quinton Quaye <quintonquaye@Quintons-MacBook-Pro.local>
2017-10-23 18:13:45 -0500

    started a new project

--- FoodPin/ViewController.swift
+++ FoodPin/ViewController.swift
@@ -8,8 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //------------------- for outlets
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    //-------------------
+    
+    
+    //------------------- for data
+    var restaurantNames =  ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional","Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+   var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg","barrafina.jpg", "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
+    
+    
+    //-------------------
+    
+    
+
+    //------------------- for visuals
+    
+    // for hiding the statusBar...
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    //-------------------
+    
+ 
+    //------------------- for function
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
+        
+        return cell
+    }
+    
+//-------------------
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
