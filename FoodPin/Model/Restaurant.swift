219a128e24539ccc0f03b1de295233552ae2a5f2
Quinton Quaye <quintonquaye@Quintons-MacBook-Pro.local>
2017-10-30 00:12:28 -0500

    updates from last workday

---
+++ FoodPin/Model/Restaurant.swift
@@ -0,0 +1,9 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Quinton Quaye on 10/26/17.
+//  Copyright © 2017 Quinton Quaye. All rights reserved.
+//
+
+import Foundation
---
+++ FoodPin/View/RestaurantDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantDetailTableViewCell.swift
+//  FoodPin
+//
+//  Created by Quinton Quaye on 10/27/17.
+//  Copyright © 2017 Quinton Quaye. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
